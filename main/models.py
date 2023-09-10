833bef979927105362f18d400c169ca2b011c522
Arxeno <rmfbkw04@gmail.com>
2023-09-10 22:54:03 +0700

    fix models: Item

--- main/models.py
+++ main/models.py
@@ -1,20 +1,27 @@
 from django.db import models
 import uuid
+from os.path import join
 
 # Create your models here.
 
 
 class Category(models.Model):
-    category_id = models.UUIDField(
+    id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField()
+    name = models.CharField(unique=True)
 
 
 class Item(models.Model):
-    item_id = models.UUIDField(
+    def upload_path(instance, filename):
+        ext = filename.split('.')[-1]
+
+        return join('uploads', instance.category.name, f'{instance.name}.{ext}')
+
+    id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField()
+    name = models.CharField(unique=True)
     amount = models.IntegerField()
     description = models.TextField()
     price = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_path)
