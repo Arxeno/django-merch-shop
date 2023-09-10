6118b5d85d18cb8f567d0049d09e73e7eb9bfe9a
Arxeno <rmfbkw04@gmail.com>
2023-09-10 22:00:46 +0700

    add models: Item, Category

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField()
+
+
+class Item(models.Model):
+    item_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField()
+    amount = models.IntegerField()
+    description = models.TextField()
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
