1820633a0fc211781957bfae3ab2d38bd2dbe3cc
Arxeno <rmfbkw04@gmail.com>
2023-09-11 12:47:20 +0700

    fix: url image

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display_landing)
+    path('', views.display_landing),
+    path('uploads/<str:category>/<str:filename>', views.send_image)
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404
+from .models import Item
 
 # Create your views here.
 
 
 def display_landing(request):
-    return render(request, 'index.html')
+    try:
+        items = Item.objects.all()
+    except:
+        return Http404()
+
+    context = {
+        'items': items
+    }
+    return render(request, 'index.html', context)
+
+
+def send_image(request, category, filename):
+    try:
+        file = open(f'uploads/{category}/{filename}', 'rb')
+        response = FileResponse(file)
+
+        ext = filename.split('.')[-1]
+        if (ext == 'png'):
+            response['Content-Type'] = 'image/png'
+        elif (ext in ['jpg', 'jpeg']):
+            response['Content-Type'] = 'image/jpeg'
+        elif (ext == 'webp'):
+            response['Content-Type'] = 'image/webp'
+
+        return response
+    except:
+        return Http404()
