cc348795f00568d3015abab72673897cd5aae30a
Arxeno <rmfbkw04@gmail.com>
2023-09-27 08:15:25 +0700

    feat: login

--- main/urls.py
+++ main/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display_landing),
+    path('', views.display_landing, name='display_landing'),
     path('uploads/<str:category>/<str:filename>', views.send_image),
     path('create/item', views.create_item, name='create_item'),
     path('item/xml', views.show_xml),
     path('item/xml/<str:id>', views.show_xml_by_id),
     path('item/json', views.show_json),
     path('item/json/<str:id>', views.show_json_by_id),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login')
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from .forms import ItemForm
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -93,3 +94,18 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'register.html', context)
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:display_landing')
+        else:
+            messages.info(
+                request, 'Maaf, entry username atau password salah. Silahkan coba lagi.')
+    context = {}
+    return render(request, 'login.html', context)
